60c0b5fdb8cefd453264a1a92f38fae1a36cbbf2
Vien Dinh <maxvien@gmail.com>
2025-07-31 04:16:27 +0000

    react-spa: setup shadcn

--- templates/react-spa-tantack-router/src/routes/__root.tsx
+++ templates/react-spa-tantack-router/src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex gap-2 p-2">
+      <div className="flex justify-center gap-3 p-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
--- templates/react-spa-tantack-router/src/routes/index.tsx
+++ templates/react-spa-tantack-router/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@esmate/shadcn/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,8 +7,36 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div>
+      <div className="container mx-auto px-4 py-24 md:px-6 lg:py-32 2xl:max-w-[1400px]">
+        {/* Title */}
+        <div className="mx-auto mt-5 max-w-2xl text-center">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">ESMate React</h1>
+        </div>
+        {/* End Title */}
+        <div className="mx-auto mt-5 max-w-3xl text-center">
+          <p className="text-xl text-muted-foreground">
+            Uncomplicate React Single Application Development with Vite, Tantack Router, React Query, Tailwind CSS,
+            Shaddcn UI, ESLint, Prettier, and more.
+          </p>
+        </div>
+        {/* Buttons */}
+        <div className="mt-8 flex justify-center gap-3">
+          <a href="https://github.com/viendinhcom/esmate" target="_blank" rel="noreferrer">
+            <Button size="lg">ESMate</Button>
+          </a>
+          <a
+            href="https://github.com/VienDinhCom/esmate/tree/main/templates/react-spa"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button size="lg" variant="outline">
+              Learn More
+            </Button>
+          </a>
+        </div>
+        {/* End Buttons */}
+      </div>
     </div>
   );
 }
