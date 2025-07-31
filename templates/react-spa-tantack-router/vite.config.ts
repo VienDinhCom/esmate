08d95c60be61d4212dc8c305ed09dc2489b8c470
Vien Dinh <maxvien@gmail.com>
2025-07-31 02:28:30 +0000

    react-spa: setup tsconfig-paths

--- templates/react-spa-tantack-router/vite.config.ts
+++ templates/react-spa-tantack-router/vite.config.ts
@@ -2,14 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    tanstackRouter({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tsConfigPaths(), tanstackRouter({ target: "react", autoCodeSplitting: true }), react(), tailwindcss()],
 });
