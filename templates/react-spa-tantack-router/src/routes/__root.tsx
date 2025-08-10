50abdce29c1c47444cfd79f5a9a592a3a7f55f75
Vien Dinh <maxvien@gmail.com>
2025-08-10 03:39:55 +0000

    update readme

--- templates/react-spa-tantack-router/src/routes/__root.tsx
+++ templates/react-spa-tantack-router/src/routes/__root.tsx
@@ -1,9 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+const queryClient = new QueryClient();
+
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="flex justify-center gap-3 p-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
@@ -11,10 +15,14 @@ export const Route = createRootRoute({
         <Link to="/about" className="[&.active]:font-bold">
           About
         </Link>
+        <Link to="/posts" className="[&.active]:font-bold">
+          Posts
+        </Link>
       </div>
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   ),
 });
