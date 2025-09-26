9e06ad53012c44ccdff0c113102e1df0f4155d89
Vien Dinh <maxvien@gmail.com>
2025-09-26 03:23:24 +0000

    cli: hide env messages

--- packages/cli/src/commands/task.ts
+++ packages/cli/src/commands/task.ts
@@ -1,7 +1,7 @@
+import dotenv from "@dotenvx/dotenvx";
 import { defineCommand } from "citty";
 import { consola } from "consola";
 import { isJSONObject } from "es-toolkit";
-import "@dotenvx/dotenvx/config";
 
 import { pkg } from "@src/pkg";
 import { execParallelly, execSingly } from "@src/utils";
@@ -28,6 +28,8 @@ export const task = defineCommand({
       }
 
       const isParallel = isJSONObject(task);
+
+      dotenv.config({ quiet: true });
 
       if (isParallel) {
         execParallelly(task as Record<string, string | string[]>);
