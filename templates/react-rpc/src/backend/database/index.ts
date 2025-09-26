df5ff402cff1c27800c06d2a49695d84b65ef6c5
Vien Dinh <maxvien@gmail.com>
2025-09-26 02:25:12 +0000

    react-rpc: replace pg with postgres.js

--- templates/react-rpc/src/backend/database/index.ts
+++ templates/react-rpc/src/backend/database/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema.ts";
 import { env } from "@/shared/env";
 
