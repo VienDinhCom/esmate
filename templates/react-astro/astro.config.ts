2a516d2452a3f17b2b9124ff0f8ad6a0b241f5c2
Vien Dinh <maxvien@gmail.com>
2025-09-28 10:39:20 +0000

    react-astro: config tanstack router for spa

--- templates/react-astro/astro.config.ts
+++ templates/react-astro/astro.config.ts
@@ -1,11 +1,19 @@
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), react()],
+  vite: {
+    plugins: [
+      tanstackRouter({
+        autoCodeSplitting: true,
+        generatedRouteTree: "./src/shared/integrations/spa-router/route-tree.gen.ts",
+      }),
+    ],
+  },
 });
---
+++ templates/react-astro/src/shared/integrations/spa-router/index.ts
@@ -0,0 +1,1 @@
+export { SpaRouter } from "./spa-router";
