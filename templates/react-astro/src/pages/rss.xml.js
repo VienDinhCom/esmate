8df3ed34f52da5c0d371fdac0ec983e94e53468c
Vien Dinh <maxvien@gmail.com>
2025-09-29 03:34:54 +0000

    react-astro: rename to consts

--- templates/react-astro/src/pages/rss.xml.js
+++ templates/react-astro/src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "@shared/constants";
+import { SITE_DESCRIPTION, SITE_TITLE } from "@shared/consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
--- templates/react-astro/src/routes/__root.tsx
+++ templates/react-astro/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+// import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 // import { TanstackDevtools } from '@tanstack/react-devtools'
 
 export const Route = createRootRoute({
