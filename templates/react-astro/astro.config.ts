790db46f7d6d27767349095f1386dac35de5eeca
Vien Dinh <maxvien@gmail.com>
2025-09-30 05:11:34 +0000

    react-astro: config tailwindcss and shadcn

--- templates/react-astro/astro.config.ts
+++ templates/react-astro/astro.config.ts
@@ -1,8 +1,10 @@
+// @ts-nocheck
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +13,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [
+      tailwindcss(),
       tanstackRouter({
         autoCodeSplitting: true,
         routesDirectory: "./src/frontend/tanstack/routes",
