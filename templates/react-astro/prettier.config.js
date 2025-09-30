7a1202edfc4def4f48ba58c8852977f04f36cd32
Vien Dinh <maxvien@gmail.com>
2025-09-30 11:18:44 +0000

    react-astro: config router

--- templates/react-astro/prettier.config.js
+++ templates/react-astro/prettier.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   tailwind: {
     tailwindStylesheet: "src/assets/styles/global.css",
   },
-  ignores: ["pnpm-lock.yaml", "package-lock.json"],
+  ignores: ["pnpm-lock.yaml", "package-lock.json", "src/frontend/tanstack/config/router/route-tree.gen.ts"],
 });
