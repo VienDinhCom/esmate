137a12b3adad8d0caf622f095b7e1c41772ebb1f
Vien Dinh <maxvien@gmail.com>
2025-10-03 08:12:39 +0000

    react: add useImmutableState hook

---
+++ packages/react/src/hooks/use-immutable-state.ts
@@ -0,0 +1,42 @@
+import type { Dispatch, SetStateAction } from "react";
+
+import { produce } from "immer";
+import { useState } from "react";
+
+export function useImmutableState<T>(initialState: T | (() => T)): [T, Dispatch<SetStateAction<T>>] {
+  const [state, setState] = useState<T>(initialState);
+
+  const updateState: Dispatch<SetStateAction<T>> = (action) => {
+    if (typeof action === "function") {
+      setState((prev) => produce(prev, action as (draft: T) => void | T));
+    } else {
+      setState(action);
+    }
+  };
+
+  return [state, updateState];
+}
+
+// import type { Dispatch, SetStateAction } from "react";
+
+// import { useState } from "react";
+
+// export function useImmutableState<T>(initialState: T | (() => T)): [T, Dispatch<SetStateAction<T>>] {
+//   const [state, setState] = useState<T>(initialState);
+
+//   const updateState: Dispatch<SetStateAction<T>> = (action) => {
+//     if (typeof action === "function") {
+//       setState((prev) => {
+//         const draft = structuredClone(prev);
+
+//         return (action as (prevState: T) => T)(draft);
+//       });
+//     } else {
+//       const draft = structuredClone(action);
+
+//       setState(draft);
+//     }
+//   };
+
+//   return [state, updateState];
+// }
