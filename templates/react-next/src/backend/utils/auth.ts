a32f63fd4c60a736e97b4e66953cf20542e6a95f
Vien Dinh <maxvien@gmail.com>
2025-11-28 15:38:39 +0700

    react-next: setup better auth

---
+++ templates/react-next/src/backend/utils/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { database } from "@backend/database";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(database, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  rateLimit: {
+    enabled: true,
+  },
+});
