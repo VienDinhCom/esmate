f537f547447b321a4034be07891048f4ba88e370
Vien Dinh <maxvien@gmail.com>
2025-11-29 10:43:02 +0700

    react-next: update env config

--- templates/react-next/src/shared/env.ts
+++ templates/react-next/src/shared/env.ts
@@ -1,18 +1,10 @@
 import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
 
-const stringBoolean = z.coerce
-  .string()
-  .default("false")
-  .transform((val) => val === "true");
-
 // https://env.t3.gg/docs/nextjs
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
-    DATABASE_SEEDING: stringBoolean,
-    DATABASE_MIGRATING: stringBoolean,
-
     BETTER_AUTH_URL: z.url(),
     BETTER_AUTH_SECRET: z.string(),
   },
