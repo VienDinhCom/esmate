e45ffc6cc519d5a5875c0c8fc7ac42a0d7146b9d
Vien Dinh <maxvien@gmail.com>
2025-12-02 20:00:02 +0700

    react-next: update stripe checkout

--- templates/react-next/src/app/api/stripe/checkout/route.ts
+++ templates/react-next/src/app/api/stripe/checkout/route.ts
@@ -1,7 +1,6 @@
 import { eq } from "drizzle-orm";
-import { db } from "@/lib/db/drizzle";
-import { users, teams, teamMembers } from "@/lib/db/schema";
-import { setSession } from "@/lib/auth/session";
+import { db } from "@/lib/db";
+import { user } from "@/lib/db/schema";
 import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/payments";
 import Stripe from "stripe";
@@ -51,30 +50,8 @@ export async function GET(request: NextRequest) {
       throw new Error("No user ID found in session's client_reference_id.");
     }
 
-    const user = await db
-      .select()
-      .from(users)
-      .where(eq(users.id, Number(userId)))
-      .limit(1);
-
-    if (user.length === 0) {
-      throw new Error("User not found in database.");
-    }
-
-    const userTeam = await db
-      .select({
-        teamId: teamMembers.teamId,
-      })
-      .from(teamMembers)
-      .where(eq(teamMembers.userId, user[0].id))
-      .limit(1);
-
-    if (userTeam.length === 0) {
-      throw new Error("User is not associated with any team.");
-    }
-
     await db
-      .update(teams)
+      .update(user)
       .set({
         stripeCustomerId: customerId,
         stripeSubscriptionId: subscriptionId,
@@ -83,9 +60,8 @@ export async function GET(request: NextRequest) {
         subscriptionStatus: subscription.status,
         updatedAt: new Date(),
       })
-      .where(eq(teams.id, userTeam[0].teamId));
+      .where(eq(user.id, userId));
 
-    await setSession(user[0]);
     return NextResponse.redirect(new URL("/dashboard", request.url));
   } catch (error) {
     console.error("Error handling successful checkout:", error);
