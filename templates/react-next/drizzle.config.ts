4cf57af8becedd91b19d27c204e987360b9d01cb
Vien Dinh <maxvien@gmail.com>
2025-12-03 10:37:41 +0700

    react-next: update database

--- templates/react-next/drizzle.config.ts
+++ templates/react-next/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   casing: "snake_case",
   dialect: "postgresql",
   out: "src/lib/db/migrations",
-  schema: "src/lib/db/index.ts",
+  schema: "src/lib/db/schema/index.ts",
   dbCredentials: { url: env.DATABASE_URL },
 });
