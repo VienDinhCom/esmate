b410e0df7e60e69136b2a04106679b396e509d25
Vien Dinh <maxvien@gmail.com>
2025-12-04 12:22:47 +0700

    react-next: config toaster

--- templates/react-next/src/app/layout.tsx
+++ templates/react-next/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Manrope } from "next/font/google";
 import type { Metadata, Viewport } from "next";
+import { Toaster } from "@esmate/shadcn/pkgs/sonner";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,11 +16,11 @@ const manrope = Manrope({ subsets: ["latin"] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
-    >
-      <body className="min-h-[100dvh] bg-gray-50">{children}</body>
+    <html lang="en" className={`bg-white text-black dark:bg-gray-950 dark:text-white ${manrope.className}`}>
+      <body className="min-h-[100dvh] bg-gray-50">
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
