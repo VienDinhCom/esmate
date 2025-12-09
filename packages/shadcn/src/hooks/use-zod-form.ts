7e9b87b2da7b380f2d74a0115406ffa1cd141015
Vien Dinh <maxvien@gmail.com>
2025-12-09 12:55:10 +0700

    shadcn: fix zod form hook

--- packages/shadcn/src/hooks/use-zod-form.ts
+++ packages/shadcn/src/hooks/use-zod-form.ts
@@ -1,29 +1,33 @@
-import { useForm, UseFormProps, UseFormReturn } from "react-hook-form";
+import {
+  useForm,
+  type UseFormProps,
+  type UseFormReturn,
+} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z, type ZodType } from "zod";
+import { type ZodType, z } from "zod";
 
-/**
- * A custom hook that wraps useForm and automatically
- * applies the zodResolver using the provided Zod schema.
- *
- * @param {object} props - The form properties, including the Zod schema.
- * @param {ZodType<TFieldValues>} props.schema - The Zod schema defining the form fields.
- * @param {UseFormProps<TFieldValues>} [props.formProps] - Optional standard react-hook-form UseFormProps.
- * @returns {UseFormReturn<TFieldValues>} The result of useForm.
- */
-export const useZodForm = <TFieldValues extends z.AnyZodObject>(
-  props: {
-    schema: TFieldValues;
-  } & UseFormProps<z.infer<TFieldValues>>
-): UseFormReturn<z.infer<TFieldValues>> => {
-  type FormData = z.infer<TFieldValues>;
-
-  // Destructure the schema and spread the rest of the standard useForm props
+export function useZodForm<TSchema extends ZodType<any, any>>(
+  props: Omit<UseFormProps<z.infer<TSchema>>, "resolver"> & { schema: TSchema }
+): UseFormReturn<z.infer<TSchema>> {
   const { schema, ...formProps } = props;
 
-  return useForm<FormData>({
+  return useForm({
     ...formProps,
-    // Add the zodResolver using the provided schema
     resolver: zodResolver(schema) as any,
   });
-};
+}
+
+// function useTest() {
+//   const form = useZodForm({
+//     schema: z.object({
+//       name: z.string().min(2),
+//       age: z.number().min(18),
+//     }),
+//     defaultValues: {
+//       name: "John Doe",
+//       age: 18,
+//     },
+//   });
+
+//   form.register("name");
+// }
