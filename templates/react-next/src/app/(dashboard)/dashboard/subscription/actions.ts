622bd9fb4a4b63c879896c55836fd4d898c15796
Vien Dinh <maxvien@gmail.com>
2025-12-22 21:51:59 +0700

    react-next: fix manage subscription

--- templates/react-next/src/app/(dashboard)/dashboard/subscription/actions.ts
+++ templates/react-next/src/app/(dashboard)/dashboard/subscription/actions.ts
@@ -1,11 +1,14 @@
 "use server";
 
 import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 
 export async function manageSubscriptionAction() {
-  await auth.api.createBillingPortal({
-    body: { returnUrl: "/dashboard", locale: "en" },
+  const { url } = await auth.api.createBillingPortal({
+    body: { locale: "en" },
     headers: await headers(),
   });
+
+  redirect(url);
 }
