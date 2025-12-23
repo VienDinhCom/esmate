965f99a582b92bea39342a6f69b97bda7b92002c
Vien Dinh <maxvien@gmail.com>
2025-12-23 16:41:17 +0700

    utils: add fixTypos functions

--- packages/utils/src/text.ts
+++ packages/utils/src/text.ts
@@ -1,12 +1,27 @@
+import type { Configuration, Locale } from "typopo";
+
 import title from "title";
+import typopo from "typopo";
 
 /**
  * Formats a string to be used as a title.
- * Original: https://www.npmjs.com/package/title
  * @param str The string to format.
  * @param options The options to use.
  * @returns The formatted string.
  */
 export function formatTitle(str: string, options?: { special?: string[] }): string {
   return title(str, options);
+}
+
+/**
+ * Fixes typos in a string using typopo.
+ * @param str The string to fix.
+ * @param options The options to use.
+ * @returns The fixed string.
+ */
+export function fixTypos(str: string, options?: Configuration & { locale?: Locale }): string {
+  return typopo
+    .fixTypos(str, options?.locale, options)
+    .replace(/\u00A0/g, " ") // Replace non-breaking spaces with regular spaces
+    .trim();
 }
