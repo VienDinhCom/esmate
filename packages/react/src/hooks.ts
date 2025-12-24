ea9f77d3f0e033e1a7024a87bb447b60170165dd
Vien Dinh <maxvien@gmail.com>
2025-12-24 11:49:34 +0700

    react: rename `useImmutableState` to `useImmerState` and update its export.

--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -1,2 +1,2 @@
-export { useImmutableState } from "./hooks/use-immutable-state";
+export { useImmerState } from "./hooks/use-immer-state";
 export { useSearchParams } from "./hooks/use-search-params";
