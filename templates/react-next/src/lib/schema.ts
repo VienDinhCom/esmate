cf7ac3461ebb1c0c05817832df9008ace9e6b502
Vien Dinh <maxvien@gmail.com>
2025-12-30 16:42:31 +0700

    react-next: add Drizzle-Zod insert and update schemas for User.

--- templates/react-next/src/lib/schema.ts
+++ templates/react-next/src/lib/schema.ts
@@ -2,6 +2,8 @@ import { post, user } from "@/lib/db/schema";
 import { createInsertSchema, createSelectSchema, createUpdateSchema } from "drizzle-zod";
 
 export const UserSelectSchema = createSelectSchema(user);
+export const UserInsertSchema = createInsertSchema(user).omit({ id: true });
+export const UserUpdateSchema = createUpdateSchema(user).required({ id: true });
 
 export const PostSelectSchema = createSelectSchema(post);
 export const PostInsertSchema = createInsertSchema(post).omit({ id: true, authorId: true });
