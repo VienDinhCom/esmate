c1468d0ef47d2c0c27093fa47bbbbc16b4e9b9af
Vien Dinh <maxvien@gmail.com>
2025-12-31 15:05:21 +0700

    react-next: Client-side auth module to expose `authenticate` function and session utilities, and reorder server-side authorization.

--- templates/react-next/src/lib/auth/client.ts
+++ templates/react-next/src/lib/auth/client.ts
@@ -1,7 +1,49 @@
 import { createAuthClient } from "better-auth/react";
 import { stripeClient } from "@better-auth/stripe/client";
 import { adminClient } from "better-auth/client/plugins";
+import { Auth, Options, Permissions, UserRole } from "./config";
+import { invariant } from "@esmate/utils";
 
-export const authClient = createAuthClient({
+export const { getSession, admin, useSession } = createAuthClient({
   plugins: [adminClient(), stripeClient({ subscription: true })],
 });
+
+async function authenticate<P extends Permissions>(options?: Options): Promise<Omit<Auth<P>, "headers">> {
+  const { data: session } = await getSession();
+
+  async function authorize(permissions: P) {
+    const permitted = await admin.hasPermission({
+      permissions,
+    });
+
+    invariant(permitted, "User does not have permission");
+
+    return permissions;
+  }
+
+  if (!session) {
+    if (options?.callbackUrl) {
+      window.location.href = `/auth/sign-in?callbackUrl=${options.callbackUrl}`;
+    }
+
+    throw new Error("Not authenticated");
+  }
+
+  const me = {
+    id: session.user.id,
+    name: session.user.name,
+    email: session.user.email,
+    role: session.user.role as UserRole,
+  };
+
+  invariant(me.role, "User role not found");
+
+  return {
+    me,
+    authorize,
+  };
+}
+
+export const authClient = {
+  authenticate,
+};
