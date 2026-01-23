9166e070a003752564b078f2ffc22e86d04eeac0
Vien Dinh <maxvien@gmail.com>
2026-01-23 15:53:52 +0700

    react-nitro: config eslint, pritter, vitest

--- templates/react-nitro/vite.config.ts
+++ templates/react-nitro/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import { nitro } from "nitro/vite";
-import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 import { tanstackRouter as router } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
