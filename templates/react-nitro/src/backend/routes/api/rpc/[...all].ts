00904139437224be08a1e15e1d83ab96215ad4a9
Vien Dinh <maxvien@gmail.com>
2026-01-31 15:35:02 +0700

    react-nitro: Add CORS plugin to RPC handler.

--- templates/react-nitro/src/backend/routes/api/rpc/[...all].ts
+++ templates/react-nitro/src/backend/routes/api/rpc/[...all].ts
@@ -1,11 +1,13 @@
 import { onError } from "@orpc/server";
 import { RPCHandler } from "@orpc/server/fetch";
+import { CORSPlugin } from "@orpc/server/plugins";
 import { defineEventHandler } from "nitro/h3";
 
 import { createContext } from "@/backend/lib/orpc";
 import { router } from "@/backend/orpc";
 
 const handler = new RPCHandler(router, {
+  plugins: [new CORSPlugin()],
   interceptors: [
     onError((error) => {
       console.error(error);
