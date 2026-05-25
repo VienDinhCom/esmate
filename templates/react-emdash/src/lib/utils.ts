8ebac760227faaa98910b1710b209617e1cd5b89
Vien Dinh <maxvien@gmail.com>
2026-05-25 15:31:56 +0700

    refactor: move site identity logic to lib/utils and remove redundant utility file

--- templates/react-emdash/src/lib/utils.ts
+++ templates/react-emdash/src/lib/utils.ts
@@ -6,3 +6,27 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export interface MediaReference {
+  mediaId: string;
+  alt?: string;
+  url?: string;
+}
+
+export interface StarterSiteIdentitySettings {
+  title?: string;
+  tagline?: string;
+  logo?: MediaReference;
+  favicon?: MediaReference;
+}
+
+const DEFAULT_SITE_TITLE = "My Site";
+const DEFAULT_SITE_TAGLINE = "Built with EmDash";
+
+export function resolveStarterSiteIdentity(settings?: StarterSiteIdentitySettings) {
+  return {
+    siteTitle: settings?.title ?? DEFAULT_SITE_TITLE,
+    siteTagline: settings?.tagline ?? DEFAULT_SITE_TAGLINE,
+    siteLogo: settings?.logo?.url ? settings.logo : null,
+  };
+}
